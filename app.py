8e2d6ecdb8aee6dfeb9bc9449e508875712791cf
felixzhuologist <felix.czhu@gmail.com>
2016-01-30 23:24:04 +0000

    Add speech to text endpoint

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+from werkzeug import secure_filename
 from sklearn.externals import joblib
 
 from train import Trainer, UnitClassifier, CaloriesRegressor
 from predict import predict_cal, predict_units
+from speech import get_text
+from hodclient import *
 
 import time
 
@@ -15,11 +18,9 @@ calories_regressor = joblib.load('models/calories_regressor_%s.pkl'
 unit_classifier = joblib.load('models/unit_classifier_%s.pkl'
                               % time.strftime('%Y%m%d'))
 
-api_key = 'your api key'
-hpe_url = 'https://api.havenondemand.com/1/api/async/recognizespeech/v1?file={0}&apikey={1}'
-
 def speech_to_text(filename):
-  response = request.post(hpe_url.format(filename, api_key))
+  with open(filename, 'rb') as audio:
+    response = request.post(url, files={'file': audio}, data={'apikey': api_key})
   print response
 
 class Calorie(Resource):
@@ -30,6 +31,14 @@ class Calorie(Resource):
     print predicted_cals, predicted_units
     result = {'calories': int(predicted_cals[0]), 
               'units': predicted_units[0][1]}
+    return result
+
+class Speech(Resource):
+  def post(self):
+    file = request.files['file']
+    filename = secure_filename(file.filename)
+    file.save('./' + filename)
+    result = {'text': get_text(filename)}
     return result
 
 api.add_resource(Calorie, '/')
