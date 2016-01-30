272a7f63f513bd6976541299d8719d96aeac8a17
felixzhuologist <felix.czhu@gmail.com>
2016-01-30 20:56:33 +0000

    Change post to get

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_restful import Resource, Api
 from sklearn.externals import joblib
 
 from train import Trainer, UnitClassifier, CaloriesRegressor
+from predict import predict_cal, predict_units
 
 import time
 
@@ -14,21 +15,6 @@ calories_regressor = joblib.load('models/calories_regressor_%s.pkl'
 unit_classifier = joblib.load('models/unit_classifier_%s.pkl'
                               % time.strftime('%Y%m%d'))
 
-def predict_cal(predictor, dish_with_unit):
-    predicted_cal = predictor.Predict([dish_with_unit])
-    return predicted_cal
-
-def predict_units(predictor, dish_without_unit, topn):
-    predicted_units = predictor.PredictProba([dish_without_unit])
-    units = predicted_units[0]
-    predicted_units = []
-    for i, u in enumerate(units):
-        if u <= 0:
-            continue
-        predicted_units.append((u, unit_classifier._model.classes_[i]))
-    predicted_units = sorted(predicted_units, reverse = True)
-    return predicted_units[:topn]
-
 api_key = 'your api key'
 hpe_url = 'https://api.havenondemand.com/1/api/async/recognizespeech/v1?file={0}&apikey={1}'
 
@@ -38,14 +24,12 @@ def speech_to_text(filename):
 
 class Calorie(Resource):
   def get(self):
-      return {todo_id: todos[todo_id]}
-
-  def put(self):
-    predicted_cals = predict_cal(calories_regressor, request.form['dish'])
+    dish = request.args.get('dish')
+    predicted_cals = predict_cal(calories_regressor, dish)
     print predicted_cals
-    return {'calories': predicted_cals[0]}
+    return predicted_cals[0]
 
-api.add_resource(Calorie, '/calorie')
+api.add_resource(Calorie, '/')
 
 if __name__ == '__main__':
   app.run(debug=True)
