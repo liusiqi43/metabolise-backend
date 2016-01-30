c25aef9cda41ebd0a6bec627e9f972f618b1907e
felixzhuologist <felix.czhu@gmail.com>
2016-01-30 18:26:34 +0000

    Switch to flask

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
 from train import Trainer, UnitClassifier, CaloriesRegressor
 from sklearn.externals import joblib
-
 import time
+
+app = Flask(__name__)
+api = Api(app)
+
 
 def predict_cal(dish_with_unit):
     predicted_cal = calories_regressor.Predict([dish_with_unit])
@@ -18,15 +23,21 @@ def predict_units(dish_without_unit, topn):
     predicted_units = sorted(predicted_units, reverse = True)
     return predicted_units[:topn]
 
+calories_regressor = joblib.load('models/calories_regressor_%s.pkl'
+                                 % time.strftime('%Y%m%d'))
+unit_classifier = joblib.load('models/unit_classifier_%s.pkl'
+                              % time.strftime('%Y%m%d'))
 
+class Metaboliser(Resource):
+  def get(self):
+      return {todo_id: todos[todo_id]}
+
+  def put(self):
+    predicted_cals = predict_cal(request.form['dish'])
+    print predicted_cals
+    return {'calories': predicted_cals[0]}
+
+api.add_resource(Metaboliser, '/')
 
 if __name__ == '__main__':
-    calories_regressor = joblib.load('models/calories_regressor_%s.pkl'
-                                     % time.strftime('%Y%m%d'))
-    unit_classifier = joblib.load('models/unit_classifier_%s.pkl'
-                                  % time.strftime('%Y%m%d'))
-
-    while True:
-        name = raw_input('Name of the dish? ')
-        print 'calories: ', predict_cal(name)
-        print 'units: ', predict_units(name, 5)
+  app.run(debug=True)
---
+++ predict.py
@@ -0,0 +1,16 @@
+from train import Trainer, CaloriesClassifier
+from sklearn.externals import joblib
+
+import time
+
+calories_classifier = joblib.load('models/calories_classifier_%s_joblib.pkl' %
+                                  time.strftime('%Y%m%d'))
+
+def predict_cal(dish_with_unit):
+    predicted_cals = calories_classifier.Predict([dish_with_unit])
+    return predicted_cals
+
+if __name__ == '__main__':
+    while True:
+        name = raw_input('Name of the dish? ')
+        print predict_cal(name)
