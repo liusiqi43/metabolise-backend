50f426c544a195a014e753a8a50fafb4760a293d
felixzhuologist <felix.czhu@gmail.com>
2016-01-30 20:36:17 +0000

    update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask_restful import Resource, Api
 from sklearn.externals import joblib
 
 from train import Trainer, UnitClassifier, CaloriesRegressor
-from predict import predict_cal, predict_units
 
 import time
 
@@ -15,7 +14,29 @@ calories_regressor = joblib.load('models/calories_regressor_%s.pkl'
 unit_classifier = joblib.load('models/unit_classifier_%s.pkl'
                               % time.strftime('%Y%m%d'))
 
-class Metaboliser(Resource):
+def predict_cal(predictor, dish_with_unit):
+    predicted_cal = predictor.Predict([dish_with_unit])
+    return predicted_cal
+
+def predict_units(predictor, dish_without_unit, topn):
+    predicted_units = predictor.PredictProba([dish_without_unit])
+    units = predicted_units[0]
+    predicted_units = []
+    for i, u in enumerate(units):
+        if u <= 0:
+            continue
+        predicted_units.append((u, unit_classifier._model.classes_[i]))
+    predicted_units = sorted(predicted_units, reverse = True)
+    return predicted_units[:topn]
+
+api_key = 'your api key'
+hpe_url = 'https://api.havenondemand.com/1/api/async/recognizespeech/v1?file={0}&apikey={1}'
+
+def speech_to_text(filename):
+  response = request.post(hpe_url.format(filename, api_key))
+  print response
+
+class Calorie(Resource):
   def get(self):
       return {todo_id: todos[todo_id]}
 
@@ -24,7 +45,7 @@ class Metaboliser(Resource):
     print predicted_cals
     return {'calories': predicted_cals[0]}
 
-api.add_resource(Metaboliser, '/')
+api.add_resource(Calorie, '/calorie')
 
 if __name__ == '__main__':
   app.run(debug=True)
